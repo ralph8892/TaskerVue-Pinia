c2072507986972fd3a2fd05854dcf4d2481d9b91
ralph8892 <brufalralph@gmail.com>
2024-05-17 00:38:54 +0800

    Refactored Pinia stores via setup stores

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,39 +1,42 @@
+import { ref, reactive, computed } from "vue"
 import { defineStore } from "pinia";
 import { allTasks } from "../http/task-api";
 
-export const useTaskStore = defineStore('taskStore', {
+const tmp = {
     state: () => ({
-        tasks: [],
-        task: {
-            id: 1,
-            name: "First task",
-            is_completed: false
-        },
+        
     }),
     getters: {
-        completedTasks: (state) => state.tasks.filter((task) => task.is_completed),
-        uncompletedTasks () {
-            return this.tasks.filter((task) => !task.is_completed)
-        },
-        // uncompletedCount () {
-        //     return this.uncompleted.length
-        // },
+        
     },
 
     actions: {
-        async fetchAllTasks () {
-            // this.tasks = [{
-            //     id: 1,
-            //     name: "First sample task for Pinia",
-            //     is_completed: false
-            // }, {
-            //     id: 2,
-            //     name: "Second sample task for Pinia",
-            //     is_completed: true
-            // }]
-            const { data } = await allTasks()
-            this.tasks = data.data
-        },
+        
     },
     
+}
+
+export const useTaskStore = defineStore('taskStore', () => {
+    const tasks = ref([])
+
+    const uncompletedTasks = computed(() => tasks.value.filter((task) => !task.is_completed))
+    const completedTasks = computed(() => tasks.value.filter((task) => task.is_completed))
+
+    const fetchAllTasks = async () => {
+        // this.tasks = [{
+        //     id: 1,
+        //     name: "First sample task for Pinia",
+        //     is_completed: false
+        // }, {
+        //     id: 2,
+        //     name: "Second sample task for Pinia",
+        //     is_completed: true
+        // }]
+        const { data } = await allTasks();
+        this.tasks = data.data;
+    }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
+    }
 });
