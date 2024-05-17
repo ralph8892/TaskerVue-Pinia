0ee653f3769ec3ec4c962681f0c838140c1c6bcc
ralph8892 <brufalralph@gmail.com>
2024-05-17 21:37:48 +0800

    Created a route handler for non-existing pages

---
+++ src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container py-5">
+        <h1>404</h1>
+        <p>Sorry, the page <code>{{ $route.params.notFound }}</code> you're looking for does not exist!</p>
+        <router-link :to="{ name: 'tasks' }">Back to Tasks Page</router-link>
+    </div>
+</template>
